f83e1922d13da888bcede2e088d050fd03d3ed82
gustavocuore <gucuore@gmail.com>
2022-06-04 19:27:08 -0300

    Inserindo a opção de linguagem principal #7daysofcode

--- day3/day3.js
+++ day3/day3.js
@@ -1,4 +1,5 @@
 let area;
+let linguagem;
 let carreira;
 let tecnologias = [];
 
@@ -13,8 +14,24 @@ while(area != '1' && area != '2') {
 
 if (area == '1') {
     area = 'Front-end';
+    while (linguagem != 'React' && linguagem != 'Vue') {
+        linguagem = prompt('Qual linguagem deseja aprender?\n1- React\n2- Vue');
+        if(linguagem == '1') {
+            linguagem = 'React';
+        } else {
+            linguagem = 'Vue'
+        }
+    }
 } else {
     area = 'Back-end';
+    while (linguagem != 'C#' && linguagem != 'Java') {
+        linguagem = prompt('Qual linguagem deseja aprender?\n1- C#\n2- Java');
+        if(linguagem == '1') {
+            linguagem = 'C#';
+        } else {
+            linguagem = 'Java'
+        }
+    }
 }
 
 while(carreira != '1' && carreira != '2') {
@@ -23,11 +40,11 @@ while(carreira != '1' && carreira != '2') {
 
 if (carreira == '1') {
     carreira = `Especialista ${area}`;
+    alert(`Seguirá a carreira ${carreira}, com a linguagem principal ${linguagem}`);
 } else {
     carreira = 'Fullstack';
+    alert(`Seguirá a carreira ${carreira}. Para isso, será necessário aprender outras linguagens e tecnologias além de ${linguagem}`);
 }
-
-alert(`Certo. Seguirá a carreira ${carreira}`);
 
 let nova_tecnologia = prompt('Insira abaixo uma tecnologia para sua lista de aprendizado ou clique em "Cancel" para finalizar?');
 
@@ -39,4 +56,4 @@ for (let i = 0; i <= tecnologias.length; i++) {
     }
 }
 
-alert(`Área escolhida = ${area}\nCarreira = ${carreira}\nLista de aprendizado = ${tecnologias}`);
+alert(`Área escolhida = ${area}\nLinguagem principal = ${linguagem}\nCarreira = ${carreira}\nLista de aprendizado = ${tecnologias}`);
